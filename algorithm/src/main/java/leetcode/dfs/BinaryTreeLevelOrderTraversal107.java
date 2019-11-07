4fffed9320a7774aa61eed6437b9f0e52699373f
no one <L827912244@163.com>
2019-11-08 00:54:06 +0800

    feat(107):solved No.107 whit BFS

---
+++ algorithm/src/main/java/leetcode/dfs/BinaryTreeLevelOrderTraversal107.java
@@ -0,0 +1,59 @@
+package leetcode.dfs;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreeLevelOrderTraversal107 {
+
+	public List<List<Integer>> levelOrderBottom(TreeNode root) {
+		if (root == null) {
+			return new LinkedList<>();
+		}
+		LinkedList<List<Integer>> result = new LinkedList<>();
+		LinkedList<TreeNode> queue = new LinkedList<>();
+		queue.push(root);
+		TreeNode curNode;
+		List<Integer> list;
+		LinkedList<TreeNode> curLevel = new LinkedList<>();
+		while (!queue.isEmpty()) {
+			list = new ArrayList<>();
+			while(!queue.isEmpty()) {
+				curNode = queue.poll();
+				curLevel.add(curNode);
+				list.add(curNode.getVal());
+			}
+			result.push(list);
+			while (!curLevel.isEmpty()) {
+				TreeNode node = curLevel.poll();
+				//add方法是添加到队尾，因为是从左到右的顺序
+				if (node.getLeft() != null) {
+					queue.add(node.getLeft());
+				}
+				if (node.getRight() != null) {
+					queue.add(node.getRight());
+				}
+			}
+
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		TreeNode node = new TreeNode(3);
+		TreeNode node1 = new TreeNode(9);
+		TreeNode node2 = new TreeNode(20);
+		TreeNode node3 = new TreeNode(15);
+		TreeNode node4 = new TreeNode(7);
+
+		node.setLeft(node1);
+		node.setRight(node2);
+		node2.setLeft(node3);
+		node2.setRight(node4);
+
+		BinaryTreeLevelOrderTraversal107 btlot = new BinaryTreeLevelOrderTraversal107();
+
+		System.out.println(btlot.levelOrderBottom(node));
+
+	}
+}
